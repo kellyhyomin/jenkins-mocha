464c1bed32ace4dbffcc7aa40adf59c714cdd8ba
hmkim <hmkim@popcornsar.com>
2020-07-07 17:27:59 +0900

    edit para_basic_test.js
    Signed-off-by: hmkim <hmkim@popcornsar.com>

--- para_basic_test.js
+++ para_basic_test.js
@@ -35,28 +35,7 @@ describe('Test', function() {
       afterEach(async function() {
         await app.switchDefaultContent();
       });
-      it('Task: Open qemu-env terminal 1', async function() {
-          let taskLabel = 'Open qemu-env terminal 1';
-          let compareContext = 'Welcome to Ubuntu 18.04.4 LTS';
-          let expectedResult = 'No Results';
-          let menubarName = 'Terminal';
-          let subMenubarName = 'Run Task...';
-          await app.sleep(10000);
-          await app.openMenuBar(menubarName);
-          await app.openSubMenu(subMenubarName);
-  
-          await app.runTask(taskLabel);
-          await app.sleep(5000);
       
-          await app.copyTerminalTextToClipboard();
-          await app.createNewFile();
-          await app.performPasteAction();
-          assert.notEqual(await app.isTextPresentInTerminalOutput(compareContext), expectedResult);
-  
-          await app.saveFile();
-          await app.closeAllTabsInMainArea();
-  
-      })
   
       it('Task: R19-03 ara::com Generator', async function() {
           let taskLabel = 'R19-03 ara::com Generator';
