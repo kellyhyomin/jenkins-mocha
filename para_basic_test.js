1b531847ae7bce4abfbcecb39e4ff8219b2f02c5
hmkim <hmkim@popcornsar.com>
2020-07-07 15:37:13 +0900

    edit para_basic_test.js
    Signed-off-by: hmkim <hmkim@popcornsar.com>

--- para_basic_test.js
+++ para_basic_test.js
@@ -2,32 +2,17 @@ const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 const app = require('./app');
 describe('Test', function() {
-  let iframeId = 'ide-application-iframe';
-  this.timeout(180000);
+  this.timeout(600000);
   before(async function() {
-  
-    let stack = process.env.POPCORNSAR_STUDIO_STACK;
-    let username = process.env.POPCORNSAR_STUDIO_USERNAME;
-    let password = process.env.POPCORNSAR_STUDIO_PASSWORD;
     let url = process.env.POPCORNSAR_STUDIO_URL;
-
     await app.init(url);
     await app.maximizeBrowser();
-    
-    //await app.selectWorkspace(stack);
-   
-  });
-  beforeEach(async function() {
-    //await app.switchIFrame(iframeId);
   });
   after(async function() {
     await app.quit();
   });
-  afterEach(async function() {
-    //await app.switchDefaultContent();
-  });
- 
-    describe('PARA Basic Test', function() { 
+
+    describe('CHE TEST', function() { 
         it('Test: LOGIN', async function() {
           let username = process.env.POPCORNSAR_STUDIO_USERNAME;
           let password = process.env.POPCORNSAR_STUDIO_PASSWORD;
@@ -38,13 +23,87 @@ describe('Test', function() {
           let stack = process.env.POPCORNSAR_STUDIO_STACK;
           await app.selectWorkspace(stack);
         })
+        // workspace 없을 시 생성 
+    })
+    describe('PARA Basic Test', function() { 
+      beforeEach(async function() {
+        let iframeId = 'ide-application-iframe';
+        await app.switchIFrame(iframeId);
+      });
+        
+      afterEach(async function() {
+        await app.switchDefaultContent();
+      });
+      it('Task: Open qemu-env terminal 1', async function() {
+          let taskLabel = 'Open qemu-env terminal 1';
+          let compareContext = 'Welcome to Ubuntu 18.04.4 LTS';
+          let expectedResult = 'No Results';
+          let menubarName = 'Terminal';
+          let subMenubarName = 'Run Task...';
+          await app.sleep(6000);
+          await app.openMenuBar(menubarName);
+          await app.openSubMenu(subMenubarName);
+  
+          await app.runTask(taskLabel);
+          await app.sleep(5000);
       
+          await app.copyTerminalTextToClipboard();
+          await app.createNewFile();
+          await app.performPasteAction();
+          assert.notEqual(await app.isTextPresentInTerminalOutput(compareContext), expectedResult);
+  
+          await app.saveFile();
+          await app.closeAllTabsInMainArea();
+  
+      })
+  
+      it('Task: R19-03 ara::com Generator', async function() {
+          let taskLabel = 'R19-03 ara::com Generator';
+          let compareContext = 'EXIT PROGRAM';
+          let expectedResult = 'No Results';
+          let menubarName = 'Terminal';
+          let subMenubarName = 'Run Task...';
+          await app.sleep(2000);
+          await app.openMenuBar(menubarName);
+          await app.openSubMenu(subMenubarName);
+          
+          await app.runTask(taskLabel);
+          await app.sleep(20000);
       
+          await app.copyTerminalTextToClipboard();
+          await app.createNewFile();
+          await app.performPasteAction();
+          assert.notEqual(await app.isTextPresentInTerminalOutput(compareContext), expectedResult);
+  
+          await app.saveFile();
+          await app.closeAllTabsInMainArea();
+  
+      })
+  
+      it('Task: R19-03 ara::exec Generator', async function() {
+          let taskLabel = 'R19-03 ara::exec Generator';
+          let compareContext = 'EXIT PROGRAM';
+          let expectedResult = 'No Results';
+          let menubarName = 'Terminal';
+          let subMenubarName = 'Run Task...';
+          await app.sleep(2000);
+          await app.openMenuBar(menubarName);
+          await app.openSubMenu(subMenubarName);
+  
+          await app.runTask(taskLabel);
+          await app.sleep(20000);
+      
+          await app.copyTerminalTextToClipboard();
+          await app.createNewFile();
+          await app.performPasteAction();
+          assert.notEqual(await app.isTextPresentInTerminalOutput(compareContext), expectedResult);
+  
+          await app.saveFile();
+          await app.closeAllTabsInMainArea();
+  
+      })  
+    })
 
-
-
-
-  })
   
 
 })
