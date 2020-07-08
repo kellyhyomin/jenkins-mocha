5627927fb6451fe59048dbf027ac35ae873b9e60
hmkim <hmkim@popcornsar.com>
2020-07-08 11:26:48 +0900

    edit para_basic_test.js
    Signed-off-by: hmkim <hmkim@popcornsar.com>

--- para_basic_test.js
+++ para_basic_test.js
@@ -43,7 +43,7 @@ describe('Test', function() {
           let expectedResult = 'No Results';
           let menubarName = 'Terminal';
           let subMenubarName = 'Run Task...';
-          await app.sleep(2000);
+          await app.sleep(5000);
           await app.openMenuBar(menubarName);
           await app.openSubMenu(subMenubarName);
           
@@ -54,7 +54,7 @@ describe('Test', function() {
           await app.createNewFile();
           await app.performPasteAction();
           assert.notEqual(await app.isTextPresentInTerminalOutput(compareContext), expectedResult);
-          await app.sleep(2000);
+          await app.sleep(5000);
           await app.saveFile();
           await app.closeAllTabsInMainArea();
   
