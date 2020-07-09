d0749622d5712b1dce7cd9ed8d25c8f96591d2ef
hmkim <hmkim@popcornsar.com>
2020-07-09 11:10:36 +0900

    edit para_basic_test.js
    Signed-off-by: hmkim <hmkim@popcornsar.com>

--- para_basic_test.js
+++ para_basic_test.js
@@ -48,7 +48,7 @@ describe('Test', function() {
         await app.openSubMenu(subMenubarName);
         
         await app.runTask(taskLabel);
-        await app.sleep(20000);
+        await app.sleep(40000);
     
         await app.copyTerminalTextToClipboard();
         await app.createNewFile();
@@ -58,7 +58,6 @@ describe('Test', function() {
         await app.saveFile();
         await app.closeAllTabsInMainArea();
 
-        
   
       })
   
