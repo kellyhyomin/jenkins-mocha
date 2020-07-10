2f16b99b3061446d8b9b426b27c8a87300bf1173
hmkim <hmkim@popcornsar.com>
2020-07-10 10:54:58 +0900

    edit para_basic_test.js
    Signed-off-by: hmkim <hmkim@popcornsar.com>

--- para_basic_test.js
+++ para_basic_test.js
@@ -6,7 +6,7 @@ describe('Test', function() {
   before(async function() {
     let url = process.env.POPCORNSAR_STUDIO_URL;
     await app.init(url);
-    //await app.maximizeBrowser();
+    await app.maximizeBrowser();
   });
   after(async function() {
     await app.quit();
