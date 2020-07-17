6575a8488a344c2e21a4432a22329eccc8a016be
hmkim <hmkim@popcornsar.com>
2020-07-17 15:07:07 +0900

    edit para_basic_test.js
    Signed-off-by: hmkim <hmkim@popcornsar.com>

--- para_basic_test.js
+++ para_basic_test.js
@@ -38,7 +38,28 @@ describe('Test', function() {
         await app.switchDefaultContent();
       });
       
-     
+      it('Task: Open qemu-env terminal 1', async function() {
+        let taskLabel = 'Open qemu-env terminal 1';
+        let compareContext = 'Welcome to Ubuntu 18.04.4 LTS';
+        let expectedResult = 'No Results';
+        let menubarName = 'Terminal';
+        let subMenubarName = 'Run Task...';
+        await app.sleep(6000);
+        await app.openMenuBar(menubarName);
+        await app.openSubMenu(subMenubarName);
+
+        await app.runTask(taskLabel);
+        await app.sleep(5000);
+    
+        await app.copyTerminalTextToClipboard();
+        await app.createNewFile();
+        await app.performPasteAction();
+        assert.notEqual(await app.isTextPresentInTerminalOutput(compareContext), expectedResult);
+
+        await app.saveFile();
+        await app.closeAllTabsInMainArea();
+
+      })
       it('Task: R19-03 ara::com Generator', async function() {
         let taskLabel = 'R19-03 ara::com Generator';
         let compareContext = 'EXIT PROGRAM';
@@ -65,30 +86,29 @@ describe('Test', function() {
 
   
       })
-  
-      /* it('Task: R19-03 ara::exec Generator', async function() {
-          let taskLabel = 'R19-03 ara::exec Generator';
-          let compareContext = 'EXIT PROGRAM';
-          let expectedResult = 'No Results';
-          let menubarName = 'Terminal';
-          let subMenubarName = 'Run Task...';
-          await app.sleep(2000);
-          await app.openMenuBar(menubarName);
-          await app.openSubMenu(subMenubarName);
-  
-          await app.runTask(taskLabel);
-          await app.sleep(20000);
-      
-          await app.copyTerminalTextToClipboard();
-          await app.createNewFile();
-          await app.performPasteAction();
-          assert.notEqual(await app.isTextPresentInTerminalOutput(compareContext), expectedResult);
-  
-          await app.saveFile();
-          await app.sleep(2000);
-          await app.closeAllTabsInMainArea();
-  
-      })   */
+      it('Task: R19-03 ara::exec Generator', async function() {
+        let taskLabel = 'R19-03 ara::exec Generator';
+        let compareContext = 'EXIT PROGRAM';
+        let expectedResult = 'No Results';
+        let menubarName = 'Terminal';
+        let subMenubarName = 'Run Task...';
+        await app.sleep(2000);
+        await app.openMenuBar(menubarName);
+        await app.openSubMenu(subMenubarName);
+
+        await app.runTask(taskLabel);
+        await app.sleep(20000);
+    
+        await app.copyTerminalTextToClipboard();
+        await app.createNewFile();
+        await app.performPasteAction();
+        assert.notEqual(await app.isTextPresentInTerminalOutput(compareContext), expectedResult);
+
+        await app.saveFile();
+        await app.closeAllTabsInMainArea();
+
+     })   
+
     })
 
   
