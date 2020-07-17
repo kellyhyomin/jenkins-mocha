ea8458f21bdf4881d7b39672c5bf66a28ee34a13
hmkim <hmkim@popcornsar.com>
2020-07-17 14:18:40 +0900

    edit app.js
    Signed-off-by: hmkim <hmkim@popcornsar.com>

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const chrome = require('selenium-webdriver/chrome');
 const { Builder, By, Key, until } = require('selenium-webdriver');
 let driver = new Builder().forBrowser('chrome').setChromeOptions(
-  new chrome.Options().addArguments(['--headless', '--no-sandbox', '--window-size=1920,1080'])).build();
+  new chrome.Options().addArguments(['--headless', '--no-sandbox', 'start-maximized'])).build();
 let activeElement = driver.switchTo().activeElement();
 
 
