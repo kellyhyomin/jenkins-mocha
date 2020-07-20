b021ddd42ad097030e67413b9fe3599a872b5749
hmkim <hmkim@popcornsar.com>
2020-07-20 10:52:49 +0900

    edit para basic test
    Signed-off-by: hmkim <hmkim@popcornsar.com>

--- para_basic_test.js
+++ para_basic_test.js
@@ -117,7 +117,7 @@ describe('Test', function() {
     
           
           
-          it('Test5: SSH qemu', async function() {
+          /* it('Test5: SSH qemu', async function() {
             let menubarName = 'Terminal';
             let subMenubarName = 'New Terminal';
             let tabId = 'shell-tab-explorer-view-container';
@@ -137,7 +137,77 @@ describe('Test', function() {
             await app.saveFile();
             await app.closeAllTabsInMainArea();
             //await app.exitTerminal();
+          }) */
+
+          it('Test6: check CPATH', async function() {
+            let menubarName = 'Terminal';
+            let subMenubarName = 'New Terminal';
+            let command = 'echo $CPATH';
+            let compareContext = '/opt/para-sdk/sysroots/core2-64-poky-linux/usr/include/c++/8.2.0/x86_64-poky-linux:/opt/para-sdk/sysroots/core2-64-poky-linux/usr/include/c++/8.2.0:/opt/para-sdk/sysroots/core2-64-poky-linux/usr/include:';
+            let expectedResult = 'No Results';
+            await app.openMenuBar(menubarName);
+            await app.openSubMenu(subMenubarName);
+            await app.sleep(3000);
+            await app.execCommand(command);
+            await app.copyTerminalTextToClipboard();
+            await app.createNewFile();
+            await app.performPasteAction();
+            assert.notEqual(await app.isTextPresentInTerminalOutput(compareContext), expectedResult);
+            await app.saveFile();
+            await app.closeAllTabsInMainArea();
+            //await app.exitTerminal();
           })
+    
+        /* it('Test7: cpp debugging', async function() {
+          let newFileName = 'hello.cpp';
+          let input = '#include <iostream> \n using std::cout; \n using std::endl; \n int main() \n { \n cout << "Hello, World!" << endl; \n return 0; \n ';
+          let menubarName = 'Terminal';
+          let subMenubarName = 'New Terminal';
+          let menubarName2 = 'Debug';
+          let subMenubarName2 = 'Remove All Breakpoints';
+          let command1 = 'unset CPATH';
+          let command2 = 'g++ -g hello.cpp -o hello.out';
+          let tabId = 'shell-tab-debug';
+          let debugOptionVal = 'C++14 debug__CONF__file:///projects';
+          let lineNumber = '5';
+          let expectedResult = 'PAUSED ON BREAKPOINT';
+          let menubarName3 = 'View';
+          let subMenubarName3 = 'Explorer';
+          
+          await app.createNewFile(newFileName);
+          await app.inputText(input);
+    
+          await app.openMenuBar(menubarName);
+          await app.openSubMenu(subMenubarName); 
+    
+          await app.execCommand(command1);
+          await app.execCommand(command2);
+          await app.sleep(1000);
+          await app.openTab(tabId);
+          await app.addBreakPoint(lineNumber);
+          await app.startDebugging(debugOptionVal);
+          await app.sleep(8000);
+          assert.equal(await app.checkPauseBreakPoint(), expectedResult);
+    
+          await app.stopDebugging();
+          await app.sleep(2000);
+          await app.openMenuBar(menubarName2);
+          await app.openSubMenu(subMenubarName2); 
+          await app.openTab(tabId);
+        })
+    
+        it('Test8: ara-api auto complete check', async function() {
+          let fileName = 'main.cpp';
+          let fileTree = 'PARA1903_sample/para_PAA/em';
+          let lineNumber = '22'
+          let expectedResult =  ' phm'
+            
+          await app.findFile(fileName, fileTree);
+          await app.sleep(3000);
+          //await app.pageDown();
+          assert.equal(await app.checkAraAutoComplete(lineNumber), expectedResult);
+        })
+ */
     
       
           
