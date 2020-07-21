07735d154dafced103bf4a152f48ed58f981c3c4
hmkim <hmkim@popcornsar.com>
2020-07-21 13:03:22 +0900

    edit apd test
    Signed-off-by: hmkim <hmkim@popcornsar.com>

--- apd_test_task.js
+++ apd_test_task.js
@@ -229,7 +229,6 @@ describe('Test', function() {
         await app.pageDown();
         await app.pageDown();
         await app.pageDown();
-        await app.pageDown();
         await app.addBreakPoint(lineNumber);
         try {
           await app.startDebugging(debugOptionVal);
