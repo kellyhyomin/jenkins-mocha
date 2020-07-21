37ef784efeb2a67721041b01b9743cb0a0dcc3c3
hmkim <hmkim@popcornsar.com>
2020-07-21 15:41:07 +0900

    edit para basic test
    Signed-off-by: hmkim <hmkim@popcornsar.com>

--- para_basic_test.js
+++ para_basic_test.js
@@ -36,7 +36,7 @@ describe('Test', function() {
         afterEach(async function() {
           await app.switchDefaultContent();
         });
-        /* it('Test1: check projects directory', async function() {
+        it('Test1: check projects directory', async function() {
             let menubarName = 'View';
             let subMenubarName = 'Explorer'
             let dirname1 = '.theia';
@@ -95,7 +95,7 @@ describe('Test', function() {
     
             await app.closeTabExplorer();
             await app.closeAllTabsInMainArea();
-          }) */
+          })
     
          /*  it('Test4: problems check' , async function() {
             let fileName = 'global_supervision.cpp';
@@ -117,7 +117,7 @@ describe('Test', function() {
     
           
           
-          /* it('Test5: SSH qemu', async function() {
+          it('Test5: SSH qemu', async function() {
             let menubarName = 'Terminal';
             let subMenubarName = 'New Terminal';
             let tabId = 'shell-tab-explorer-view-container';
@@ -194,13 +194,13 @@ describe('Test', function() {
           await app.openMenuBar(menubarName2);
           await app.openSubMenu(subMenubarName2); 
           await app.openTab(tabId);
-        }) */
+        })
     
         it('Test8: ara-api auto complete check', async function() {
           let fileName = 'main.cpp';
           let fileTree = 'PARA1903_sample/para_PAA/em';
           let lineNumber = '22'
-          let expectedResult =  ' phm'
+          let expectedResult =  ' osi'
             
           await app.findFile(fileName, fileTree);
           await app.sleep(3000);
