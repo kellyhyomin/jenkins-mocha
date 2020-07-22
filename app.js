bc0c2ccf59865feb07eb14d358d87363885a6bd5
hmkim <hmkim@popcornsar.com>
2020-07-22 17:47:01 +0900

    edit para test
    Signed-off-by: hmkim <hmkim@popcornsar.com>

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ module.exports = {
     await driver.wait(until.elementLocated(By.css(TITLE_CSS)), TS_SELENIUM_LOAD_PAGE_TIMEOUT);
     await driver.wait(until.elementLocated(By.css(HIDDEN_LOADER_CSS)), TS_SELENIUM_LOAD_PAGE_TIMEOUT);
   },
-  selectStack: async function(stackId) {
+  /* selectStack: async function(stackId) {
     const stackLocator = By.css(this.getStackCssLocator(stackId));
     await driver.wait(until.elementLocated(By.css(`span[devfile-name='${stackId}']`))).click();
   },
@@ -80,11 +80,11 @@ module.exports = {
     const ideFrameLocator = By.xpath('//ide-iframe[@id=\'ide-iframe-window\' and @aria-hidden=\'false\']');
     await driver.wait(until.elementLocated(By.xpath(CREATE_AND_OPEN_BUTTON_XPATH))).click();
     await driver.wait(until.elementLocated(By.xpath(ideFrameLocator)));
-  },
+  }, */
   createAndOpenWorkspace: async function(stackId) {
     let CREATE_AND_OPEN_BUTTON_XPATH = '(//che-button-save-flat[@che-button-title=\'Create & Open\']/button)[1]';
     const ideFrameLocator = By.xpath('//ide-iframe[@id=\'ide-iframe-window\' and @aria-hidden=\'false\']');
-    await driver.wait(until.elementLocated(By.css('span[devfile-name="' + stackId + '"]'))).click();
+    await driver.wait(until.elementLocated(By.css("span[devfile-name='" + stackId + "']"))).click();
     await driver.wait(until.elementLocated(By.xpath(CREATE_AND_OPEN_BUTTON_XPATH))).click();
     await driver.wait(until.elementLocated(By.xpath(ideFrameLocator)));
 
